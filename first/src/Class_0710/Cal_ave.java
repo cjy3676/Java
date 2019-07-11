7b2896df2c03657b261efbbad3dc3b57a0354209
cjy3676 <cjy3676@naver.com>
2019-07-11 09:37:42 +0900

    0711

---
+++ first/src/Class_0710/Cal_ave.java
@@ -0,0 +1,21 @@
+package Class_0710;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Cal_ave extends JFrame implements ActionListener {
+	TextField num1 = new TextField(5);
+	TextField num2 = new TextField(5);
+	TextField result = new TextField(5);
+	Button btn1 = new Button("¥ı«œ±‚");
+	
+	Cal_ave() {
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
